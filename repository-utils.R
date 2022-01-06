f52b7ca4b66ccea65a74c549a7b6d81f0b7f626b
matthh9797sky <matthew.henderson2@sky.uk>
2022-01-06 18:19:25 +0000

    Initial commit

---
+++ repository-utils.R
@@ -0,0 +1,13 @@
+
+
+
+combine_dfs_in_repo <- function(repo) {
+  old_dfl <- list()
+  
+  for (file in list.files(repo)) {
+    new_df <- read_csv(str_interp('${repo}/${file}.csv'))
+    old_dfl <- c(old_dfl, list(new_df))
+  }
+  
+  bind_rows(old_dfl)
+}
